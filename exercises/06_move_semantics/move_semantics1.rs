1f38352762d40b4bd51a4daa7ce22530065caf11
Krane <freesme@163.com>
2024-09-26 16:10:56 +0800

    update

--- exercises/06_move_semantics/move_semantics1.rs
+++ exercises/06_move_semantics/move_semantics1.rs
@@ -6,7 +6,16 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
 }
 
 fn main() {
-    // You can optionally experiment here.
+    // move semantics 移动语义
+
+    // 可变引用
+    let mut s = String::from("Hello");
+    change(&mut s);
+    println!("{}", s)
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(",world")
 }
 
 #[cfg(test)]
@@ -15,6 +24,7 @@ mod tests {
 
     #[test]
     fn move_semantics1() {
+        let vec0 = vec![22, 44, 66];
         let vec0 = vec![22, 44, 66];
         let vec1 = fill_vec(vec0);
         assert_eq!(vec1, vec![22, 44, 66, 88]);
